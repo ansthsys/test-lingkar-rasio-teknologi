51f3395560508704334ce6669550c1e84f86c1c9
ansthsys <azmiythufail91@gmail.com>
2024-08-30 02:19:52 +0700

    chore: install dependencies

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,21 +1,45 @@
-import './bootstrap';
-import '../css/app.css';
+import "./bootstrap";
+import "../css/app.css";
+import "preline";
+import "preline/preline";
 
-import { createRoot } from 'react-dom/client';
-import { createInertiaApp } from '@inertiajs/react';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createRoot } from "react-dom/client";
+import { createInertiaApp } from "@inertiajs/react";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { HSStaticMethods } from "preline";
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+HSStaticMethods.autoInit();
+
+const appName = import.meta.env.VITE_APP_NAME || "Laravel";
+
+const observer = new MutationObserver((mutationsList) => {
+    for (const mutation of mutationsList) {
+        HSStaticMethods.autoInit();
+    }
+});
+
+observer.observe(document.body, {
+    attributes: true,
+    subtree: true,
+    childList: true,
+    characterData: true,
+});
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.tsx`, import.meta.glob('./Pages/**/*.tsx')),
+    title: (title) => (title ? `${title} - ${appName}` : appName),
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.tsx`,
+            import.meta.glob("./Pages/**/*.tsx")
+        ),
     setup({ el, App, props }) {
         const root = createRoot(el);
 
         root.render(<App {...props} />);
+
+        delete el.dataset.page;
     },
     progress: {
-        color: '#4B5563',
+        color: "#4B5563",
     },
 });
